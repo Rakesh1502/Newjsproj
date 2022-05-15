99a3084786e936e55aaa815583a98ec5dff465ee
Rakesh1502 <rakeshjampala15@gmail.com>
2022-05-15 14:17:11 +0530

    addfile

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-let num1 = 8
-let num2 = 2
-
-document.getElementById("num-1").textContent = num1
-document.getElementById("num-2").textContent = num2
-
-let total= document.getElementById("total-1")
 
 
-function add(){
-    let res = num1 + num2
-    total.textContent= "Total : " + res
-}
 
-function subtract(){
-    let res = num1 - num2
-    total.textContent= "Total : " + res
-}
+let errorPara = document.getElementById("error")
 
-function multiply(){
-    let res = num1 * num2
-    total.textContent= "Total : " + res
-}
 
-function divide(){
-    let res = num1 / num2
-    total.textContent= "Total : " + res
+
+
+function buy() {
+    errorPara.textContent = "Oops something went wrong!! please try later"
 }
